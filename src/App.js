69518018dc0e64e0eb9e1d30e6867c6fd7cceae8
Mohammad kamalipour <mohammad.kp8093@gmail.com>
2022-04-03 18:23:46 +0430

    calander initialized and redesigned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Calendar } from "react-multi-date-picker";
+import persian from "react-date-object/calendars/persian";
+import persian_fa from "react-date-object/locales/persian_fa";
+import weekends from "react-multi-date-picker/plugins/highlight_weekends";
+import "./style.scss";
 function App() {
+  const normalDayPrice = 1500;
+  const fridayPrice = 2000;
+  const weekDays = [
+    "شنبه",
+    "یکشنبه",
+    "دوشنبه",
+    "سه شنبه",
+    "چهارشنبه",
+    "پنجشنبه",
+    "جمعه",
+  ];
+  String.prototype.toFaDigit = function () {
+    return this.replace(/\d+/g, function (digit) {
+      var ret = "";
+      for (var i = 0, len = digit.length; i < len; i++) {
+        ret += String.fromCharCode(digit.charCodeAt(i) + 1728);
+      }
+      return ret;
+    });
+  };
+  const bookedDays = ["1401/01/15", "1401/01/20", "1401/01/18", "1401/01/25"];
+  const newBookedDays = [];
+  bookedDays.map((day) => {
+    newBookedDays.push(day.toFaDigit());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Calendar
+      className="custom-calendar"
+      weekDays={weekDays}
+      calendar={persian}
+      locale={persian_fa}
+      plugins={[weekends()]}
+      mapDays={({ date }) => {
+        let isWeekend = date.weekDay.index === 6;
+        if (newBookedDays.includes(date.format("YYYY/MM/DD")))
+          return {
+            disabled: true,
+            style: { color: "#ccc" },
+            onClick: () => alert("این تاریخ رزرو شده است"),
+          };
+
+        return {
+          children: (
+            <div className="custom-day-wrapper">
+              <p className="custom-day">{date.format("D")}</p>
+              <small className="custom-day-price">
+                {isWeekend ? fridayPrice.toLocaleString() : normalDayPrice.toLocaleString()}
+              </small>
+            </div>
+          ),
+        };
+      }}
+      hideYear
+      disableYearPicker
+    />
   );
 }
 
